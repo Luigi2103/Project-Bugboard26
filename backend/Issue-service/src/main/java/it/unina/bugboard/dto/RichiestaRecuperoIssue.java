fb7e0ae907581c37b66d827fc4effe33294a03c6
Luigi Differente <lu.differente@studenti.unina.it>
2025-12-12 16:27:16 +0100

    inizio servizio recupero issue

---
+++ backend/Issue-service/src/main/java/it/unina/bugboard/dto/RichiestaRecuperoIssue.java
@@ -0,0 +1,23 @@
+package it.unina.bugboard.dto;
+
+public class RichiestaRecuperoIssue {
+    private Integer idProgetto;
+    private Integer idAssegnatario;
+
+    public Integer getIdProgetto() {
+        return idProgetto;
+    }
+
+    public void setIdProgetto(Integer idProgetto) {
+        this.idProgetto = idProgetto;
+    }
+
+    public Integer getIdAssegnatario() {
+        return idAssegnatario;
+    }
+
+    public void setIdAssegnatario(Integer idAssegnatario) {
+        this.idAssegnatario = idAssegnatario;
+    }
+
+}
