e962fbf12b3e9c531a6430740214b4034dbb1c73
FF32003 <147704016+FF32003@users.noreply.github.com>
2025-12-15 16:52:15 +0100

    aggiunto codice per gestire inserimento issue(non ancora finito)

--- frontend/src/main/java/it/unina/bugboard/inserimentoIssue/InsertIssueApiService.java
+++ frontend/src/main/java/it/unina/bugboard/inserimentoIssue/InsertIssueApiService.java
@@ -1,4 +1,76 @@
 package it.unina.bugboard.inserimentoIssue;
 
-public class InsertIssueApiService {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.unina.bugboard.inserimentoIssue.InsertIssueApiService;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InsertIssueApiService
+{
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final it.unina.bugboard.common.SessionManager sessionManager;
+
+
+    public InsertIssueApiService(it.unina.bugboard.common.SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    public RispostaIssueService inserisciIssue(String titolo, String descrizione, byte[] immagine,
+                                               String tipologia, String stato, String priorita, String istruzioni, String richiesta,
+                                               String titoloDocumento, String descrizioneDocumento, String richiestaFunzionalita,
+    LocalDate dataCreazione) {
+
+        try {
+            Map<String, Object> insertData = getMap(nome, cognome, codiceFiscale, sesso, dataNascita,
+                    username, password, email, isAdmin);
+
+            String jsonBody = objectMapper.writeValueAsString(insertData);
+            String token = this.sessionManager.getToken();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/api/inserimentoUtente"))
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer " + token)
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return objectMapper.readValue(response.body(), RispostaInserimentoUser.class);
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+            throw new InsertUserAPIException("Errore durante inserimento utente", e);
+        }
+    }
+
+
+    private static Map<String, Object> getMap(String titolo, String descrizione, byte[] immagine,
+                                              String tipologia, String stato, String priorita, String istruzioni, String richiesta,
+                                              String titoloDocumento, String descrizioneDocumento, String richiestaFunzionalita,
+                                              LocalDate dataCreazione) {
+        Map<String, Object> insertData = new HashMap<>();
+        insertData.put("titolo", titolo);
+        insertData.put("descrizione", descrizione);
+        insertData.put("immagine", immagine);
+        insertData.put("tipologia", tipologia);
+        insertData.put("stato",stato);
+        insertData.put("priorita", priorita);
+        insertData.put("istruzioni", istruzioni);
+        insertData.put("richiesta", richiesta);
+        insertData.put("titoloDocumento", titoloDocumento);
+        insertData.put("descrizioneDocumento", descrizioneDocumento);
+        insertData.put("richiestaFunzionalita", richiestaFunzionalita);
+        insertData.put("dataCreazione", dataCreazione.toString());
+
+
+
+        return insertData;
+    }
 }
+
--- frontend/src/main/java/it/unina/bugboard/inserimentoIssue/RispostaIssueService.java
+++ frontend/src/main/java/it/unina/bugboard/inserimentoIssue/RispostaIssueService.java
@@ -1,4 +1,49 @@
 package it.unina.bugboard.inserimentoIssue;
 
+import java.util.Map;
+
 public class RispostaIssueService {
+
+    private boolean success;
+    private String message;
+    private Map<String,String> fieldErrors;
+
+
+    public RispostaIssueService() {}
+
+    public RispostaIssueService(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public RispostaIssueService(boolean success, String message, Map<String, String> fieldErrors) {
+        this.success = success;
+        this.message = message;
+        this.fieldErrors = fieldErrors;
+    }
+
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getFieldErrors() {
+        return fieldErrors;
+    }
+
+    public void setFieldErrors(Map<String, String> fieldErrors) {
+        this.fieldErrors = fieldErrors;
+    }
 }
